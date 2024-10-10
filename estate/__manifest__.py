b75cf55b1bdcd2a979b56cdde6c194ac8635ebde
michneaionuc <michneaionuc@gmail.com>
2024-10-10 15:12:00 +0300

    Created property model

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,5 +1,17 @@
 {
     'name': 'Real Estate',
+    'version': '1.0',
+    'summary': 'Module for managing real estate properties',
+    'description': """
+        This module allows users to advertise properties, including features such as:
+        - Adding properties
+        - Removing properties
+        - Viewing available properties
+        - Managing sales
+    """,
     'depends': ['base'],
-    'application': True
+    'data': [],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3'
 }
