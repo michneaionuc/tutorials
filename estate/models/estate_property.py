819ed37738f65a9ab12fb6be82f63cb6ecf8b011
michneaionuc <michneaionuc@gmail.com>
2024-10-11 09:55:30 +0300

    Added UI to be able to interract with the model

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,21 +1,28 @@
 from odoo import models, fields
+from datetime import datetime, timedelta
 
 class Property(models.Model):
     _name = "estate.property"
     _description = "Property details"
 
-    name = fields.Char(required=True)
-    description = fields.Text()
-    postcode = fields.Char()
-    date_availability = fields.Date()
-    expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
-    living_area = fields.Integer()
-    facades = fields.Integer()
-    garage = fields.Boolean()
-    garden = fields.Boolean()
-    garden_area = fields.Integer()
+    name = fields.Char(
+        string = "Title", 
+        required=True)
+    description = fields.Text(string="Description")
+    postcode = fields.Char(string="Postcode")
+    date_availability = fields.Date(
+        string="Availability Date",
+        default=lambda self: datetime.today() + timedelta(days=90),
+        copy=False
+    )
+    expected_price = fields.Float(string="Expected Price", required=True)
+    selling_price = fields.Float(string="Selling Price", readonly=True, copy=False)
+    bedrooms = fields.Integer(string="Bedrooms", default=2)
+    living_area = fields.Integer(string="Living Area (sqm)")
+    facades = fields.Integer(string="Facades")
+    garage = fields.Boolean(string="Garage")
+    garden = fields.Boolean(string="Garden")
+    garden_area = fields.Integer(string="Garden Area (sqm)")
     garden_orientation = fields.Selection(
         selection=[
             ('north', 'North'),
@@ -24,4 +31,18 @@ class Property(models.Model):
             ('west', 'West')
         ],
         string="Garden Orientation"
+    ),
+    active = fields.Boolean(string="Active", default=True),
+    state = fields.Selection(
+        [
+            ('new', 'New'),
+            ('offer_received', 'Offer Received'),
+            ('offer_accepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('canceled', 'Canceled')
+        ],
+        string="State",
+        required=True,
+        copy=False,
+        default='new'
     )
